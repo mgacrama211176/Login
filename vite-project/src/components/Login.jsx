52f7d36f00e05763ff98694d09dec94d596e7b0e
MarlonG <mgacrama_ccs@uspf.edu.ph>
2023-03-10 18:30:11 +0800

    Added Backend

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Login = () => {
-  return <div>THIS IS THE LOGIN PAGE</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="" className="border flex flex-col">
+        <input type="text" />
+        <input type="text" />
+        <div className="flex gap-3">
+          <button>Submit</button>
+          <button>Register</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
